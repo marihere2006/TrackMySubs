12aede6e2c5f275f6cdecb164093a50f9ceb895b
marihere2006 <sharvesh9345603491@gmail.com>
2026-07-08 07:26:27 +0530

    Modified Frontend-Backend Cloud Configurations

--- backend/src/main/java/com/trackmysubs/config/CorsConfig.java
+++ backend/src/main/java/com/trackmysubs/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:5175")
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://trackmysubs-frontend-hosting.s3-website-us-east-1.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
